6940f9a14ac84c4318659bd1077afcbee6f5067a
DnJ0408 <oyj921205@gmail.com>
2025-02-14 21:12:35 +0900

    feat: 필요한 데이터만 가져오도록 수정하고 users(*)를 통해 관계형 데이터를 join해서 가져오기 #21

--- src/api/fetchData.js
+++ src/api/fetchData.js
@@ -2,7 +2,7 @@ import supabase from "../supabase/client";
 
 export const fetchData = async (table) => {
   try {
-    const { data } = await supabase.from(table).select("*");
+    const { data } = await supabase.from(table).select("contents, created_at, feed_id, title, users(*)");
 
     return data;
   } catch (error) {
