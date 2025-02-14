a3d89511cbc76c1b7ff52260f0c86ef1480253a7
miing_gaeng <pem4134@naver.com>
2025-02-14 17:48:11 +0900

    chore: fetchData 주석 추가 #16

--- src/api/fetchData.js
+++ src/api/fetchData.js
@@ -1,5 +1,12 @@
 import supabase from "../supabase/client";
 
+/** 해당 table의 데이터를 가져오는 로직
+ *
+ * @param {"string"} table
+ * @returns Promise객체
+ *
+ * 사용할 컴포넌트에서 async 함수로 감싼 후 사용해야합니다!
+ */
 export const fetchData = async (table) => {
   try {
     const { data } = await supabase.from(table).select("*");
