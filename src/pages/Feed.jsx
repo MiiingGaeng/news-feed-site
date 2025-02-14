3670c0b9c2468f395911cbcf0d0a6cdbb0db9116
DnJ0408 <oyj921205@gmail.com>
2025-02-14 21:11:31 +0900

    chore: 예시로 썼던 MOCK_DATA 삭제 #21

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,13 +1,11 @@
-import { useState } from "react";
 import FeedList from "../components/feed/FeedList";
-import MOCK_DATA from "../constants/MOCK_DATA";
 
 const Feed = () => {
 
-  const [posts, setPosts] = useState(MOCK_DATA);
+
   return (
     <>
-      <FeedList posts={posts} />
+      <FeedList />
     </>
 
   );
