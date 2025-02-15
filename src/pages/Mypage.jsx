15bf784c8b98afeb0ee5c11c48a60a5f3b6100f1
Aeri0730 <dodl8549@google.com>
2025-02-16 01:54:10 +0900

    feat: 기본 UI 및 Input 컴포넌트 생성

--- src/pages/Mypage.jsx
+++ src/pages/Mypage.jsx
@@ -1,39 +1,222 @@
-import { useContext, useEffect, useState } from "react";
-import { supabase } from "../supabase/client";
-import { useNavigate } from "react-router-dom";
-import { AuthContext } from "../contexts/AuthProvider";
+import { useState } from "react";
+import styled, { keyframes } from "styled-components";
+import default_img from "../assets/image/profile_default.png";
+import Button from "../common/Button";
+import Input from "../common/Input";
 
-const Mypage = () => {
-  const { setIsLogin, user, setUser } = useContext(AuthContext);
-  const [data, setData] = useState(null);
-  const navigate = useNavigate(); // NOTE: 라우터 이동
+const MyPage = () => {
+  const [data, setData] = useState({
+    id: "",
+    name: "",
+    email: "",
+    nickname: "",
+    image: "",
+  });
 
-  useEffect(() => {
-    //  user가 존재할 때만 실행
-    if (user?.email) {
-      const getUserInfo = async () => {
-        try {
-          const { data, error } = await supabase
-            .from("users") // public.users 테이블에서
-            .select("*") 
-            .eq("email", user.email) //  이메일이 같은 유저만 가져옴
-            .single(); //  단일 데이터만 가져오기
+  return (
+    <StMyPageWrapper>
+      <h1>My Page</h1>
+      <StContainer width="500px" height="300px">
+        <StProfileImg src={default_img} alt="사진없음" />
+        <Input
+          text="이메일"
+          type="email"
+          placeholder="이메일"
+          value={data.email}
+          onChangeFunc={(e) => setData({ ...data, email: e.target.value })}
+          required
+        />
+        <Input
+          text="이름"
+          type="text"
+          placeholder="이름"
+          value={data.name}
+          onChangeFunc={(e) => setData({ ...data, name: e.target.value })}
+          required
+        />
+        <Input
+          text="닉네임"
+          type="text"
+          placeholder="닉네임"
+          value={data.nickname}
+          onChangeFunc={(e) => setData({ ...data, nickname: e.target.value })}
+          required
+        />
+        <Button children="수정하기" />
+      </StContainer>
 
-          if (error) throw error;
-
-          setData(data); //  상태 업데이트
-          console.log("현재 유저 정보:", data); //  콘솔에 출력
-        } catch (error) {
-          console.error("유저 정보 가져오기 오류:", error);
-        }
-      };
-
-      getUserInfo();
-    }
-  }, [user]);
-
-
-  return <div>Mypage입니다.</div>;
+      <StContentsHeader>
+        <div></div>
+        <h2>My Contents</h2>
+        <div></div>
+      </StContentsHeader>
+      <StContentBoxWrapper>
+        {" "}
+        <StContentBox>
+          <h2>팀 프로젝트 발제</h2>
+          <p>
+            뉴스피드 프로젝트, 내가 이번학기에 참여했던 프로젝트를 정리했다.
+          </p>
+        </StContentBox>
+        <StContentBox>
+          <h2>점메추</h2>
+          <p>오늘의 점심 메뉴를 AI가 추천해준다! 이 서비스를 만들어보았다.</p>
+        </StContentBox>
+        <StContentBox>
+          <h2>리액트 9기 화이팅</h2>
+          <p>리액트 9기 동기들과 우리 다같이 성장해보자!</p>
+        </StContentBox>
+      </StContentBoxWrapper>
+    </StMyPageWrapper>
+  );
 };
 
-export default Mypage;
+const StContentBox = styled.div`
+  background: #e0e0e0;
+  padding: 20px;
+  border-radius: 10px;
+  width: 60%;
+  height: 100px;
+  text-align: center;
+  margin-left: 10px;
+  margin-bottom: 10px;
+  h2 {
+    font-size: 18px;
+    font-weight: bold;
+    margin-bottom: 10px;
+  }
+  p {
+    font-size: 14px;
+  }
+`;
+const StContentBoxWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+const StMyPageWrapper = styled.div`
+  // styles폴더에 styledComponents.js로 관리해야할것같음
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: -20px;
+  h1 {
+    font-size: 50px;
+    font-weight: bold;
+    color: #fff;
+    margin-bottom: 20px;
+  }
+`;
+
+const StContainer = styled.div`
+  // styledComponents.js로 관리해야할것같음 (height, width를 props로 받아서 사용)
+  width: ${(props) => props.width || "300px"};
+  height: ${(props) => props.height || "300px"};
+  background: #a7a5d0;
+  border-radius: 50px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  padding: 40px;
+  margin-bottom: 20px;
+  hr {
+    width: 350px;
+    opacity: 0.7;
+  }
+  form {
+    width: 100%;
+    height: 300px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    input {
+      width: 280px;
+      height: 40px;
+      border: none;
+      border-radius: 20px;
+      padding-left: 15px;
+    }
+    button {
+      width: 200px;
+      height: 40px;
+      border: none;
+      border-radius: 20px;
+      background: #504ba1;
+      color: #fff;
+      font-size: 17px;
+      transition: all 0.3s;
+      cursor: pointer;
+      &:hover {
+        background: #343434;
+      }
+    }
+  }
+`;
+
+// 스켈레톤 로딩 애니메이션 정의
+const shine = keyframes`
+  0% {
+    background-position: left -40px top 0;
+  }
+  100% {
+    background-position: right 100% top 0;
+  }
+`;
+
+// 스타일이 적용된 프로필 이미지
+const StProfileImg = styled.img`
+  display: inline-block;
+  max-width: 100%;
+  vertical-align: middle;
+  overflow-clip-margin: content-box;
+  overflow: clip;
+
+  /* 프로필 이미지 둥글게 */
+  border-radius: 30%;
+
+  /* 특정 속성을 가진 이미지 */
+  width: ${(props) => props.width || "100px"};
+  height: ${(props) => props.height || "100px"};
+  aspect-ratio: 1 / 1;
+
+  /* 스켈레톤 로딩 효과 */
+  &.skeleton {
+    background-color: #e2e5e7;
+    background-image: linear-gradient(
+      90deg,
+      rgba(255, 255, 255, 0),
+      rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0)
+    );
+    background-size: 40px 100%;
+    background-repeat: no-repeat;
+    background-position: left -40px top 0;
+    animation: ${shine} 1s ease infinite;
+  }
+`;
+
+const StContentsHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 60%;
+  margin-bottom: 20px;
+  h2 {
+    font-size: 22px;
+    font-weight: bold;
+  }
+  div {
+    flex: 1;
+    height: 2px;
+    background: #ccc;
+    margin: 0 10px;
+  }
+`;
+
+export default MyPage;
