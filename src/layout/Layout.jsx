401245f6ce378ab3e71a0ce41f119079596bb184
도니 김 <kdh4847@naver.com>
2025-02-15 15:16:37 +0900

    chore: single quote -> double quote 자동변환 #14

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,7 +1,7 @@
-import { Outlet } from 'react-router-dom';
-import styled, { keyframes } from 'styled-components';
-import Footer from './Footer';
-import Header from './Header';
+import { Outlet } from "react-router-dom";
+import styled, { keyframes } from "styled-components";
+import Footer from "./Footer";
+import Header from "./Header";
 
 const Layout = () => {
   return (
