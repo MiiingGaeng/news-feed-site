0a432285e92f3245211efabb4db3cb373ca3e173
노수민 <nosumin29@gmail.com>
2025-02-15 18:24:00 +0900

    feat: MainButton 컴포넌트 스타일링 및 Home 페이지에 통합 #29

--- src/components/common/MainButton.jsx
+++ src/components/common/MainButton.jsx
@@ -1,7 +1,28 @@
-const MainButton = () => {
+import styled from "styled-components"
+
+const MainButton = ({children}) => {
   return (
-    <div>MainButton</div>
+    <StMainButton>{children}</StMainButton>
   )
 }
+
+const StMainButton = styled.button`
+  line-height: 46px;
+  background-color: #FFF;
+  border-radius: 50px;
+  color: #000;
+  text-align: center;
+  align-items: center;
+  border: none;
+  box-sizing: border-box;
+  cursor: pointer;
+  transition: all 0.3s;
+  font-size: 1.25rem;
+  padding: 0.25rem 2.5rem;
+
+  &:hover {
+    background: #eee;
+  }
+`
 
 export default MainButton
