ed67faad74f31609a9f5884e2de0c843a6fd33f5
miing_gaeng <pem4134@naver.com>
2025-02-16 21:58:19 +0900

    chore: conflict 해결

--- src/components/feed/FeedForm.jsx
+++ src/components/feed/FeedForm.jsx
@@ -1,43 +1,150 @@
 import styled from "styled-components";
 import Button from "../../common/Button";
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import { insertOrUpdateData } from "../../api/insertOrUpdateData";
 import { FeedContext } from "../../contexts/FeedContext";
 import { fetchData } from "../../api/fetchData";
 import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { useEffect } from "react";
 
+// 초기 피드 데이터를 정의 (title과 contents는 빈 문자열로 설정)
 const INITIAL_ADD_FEED_DATA = {
   title: "",
   contents: "",
 };
 
-//-----addFeedMode 게시글 추가 컴포넌트------
-const AddFeedForm = ({ addFeedData, handleInputChange, handleAddFeed }) => (
-  <form>
-    {/* 타이틀 인풋 영역 */}
-    <StFormTitleWrapper>
-      <h1>Title</h1>
-      <StFormTitleInput
-        type="text"
-        value={addFeedData.title}
-        onChange={(e) => handleInputChange(e, "title")}
-      />
-    </StFormTitleWrapper>
-    {/* 본문 인풋 영역 */}
-    <StFormContentsWrapper>
-      <h1>Contents</h1>
-      <StFormContentsInput
-        type="text"
-        value={addFeedData.contents}
-        onChange={(e) => handleInputChange(e, "contents")}
-      />
-    </StFormContentsWrapper>
-    {/* SUBMIT 버튼 영역 */}
-    <Button type="submit" onClick={(e) => handleAddFeed(e)}>
-      글 업로드
-    </Button>
-  </form>
-);
+// 금칙어 목록을 정의. 추후 슈파베이스 테이블로 관리해도 좋을듯
+const BANNED_WORDS = ["나쁜말1", "나쁜말2", "나쁜말3"];
+
+// Feed 추가 Form 별도 분리
+const AddFeedForm = () => {
+  // FeedContext에서 toggleModal 함수를 가져옴 (모달을 열거나 닫을 때 사용)
+  const { toggleModal } = useContext(FeedContext);
+
+  // react-hook-form을 사용하여 폼 데이터 관리
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm({
+    defaultValues: INITIAL_ADD_FEED_DATA,
+  });
+
+  // 금칙어 필터링
+  const checkBannedWords = (text) => {
+    for (let i of BANNED_WORDS) {
+      if (text.includes(i)) {
+        return false;
+      }
+      return true;
+    }
+  };
+
+  // // onChange시에 event와 field 객체를 받아, input value 추가
+  // const handleInputChange = (e, field) => {
+  //   const { value } = e.target;
+  //   setAddFeedData((state) => ({
+  //     ...state,
+  //     [field]: value,
+  //   }));
+  // };
+
+  // 실제 테이블에 feed 데이터 추가하는 함수
+  const handleAddFeed = (data) => {
+    if (!data) return;
+    if (!checkBannedWords(data.title)) {
+      return alert("제목에 금칙어가 포함되어 있습니다.");
+    }
+    if (!checkBannedWords(data.contents)) {
+      return alert("내용에 금칙어가 포함되어 있습니다.");
+    }
+    // feed 데이터를 확장 (writer_id 추가)
+    const feedData = {
+      ...data,
+      writer_id: "1d4b5722-6a09-4256-9b9d-461903075838", // 예시로 writer_id 값 하드코딩
+    };
+
+    // 데이터 삽입 또는 업데이트 함수 호출
+    insertOrUpdateData(feedData, "feeds");
+    // 모달 닫기
+    toggleModal();
+
+    // 피드 추가 메시지
+    return alert("새로운 피드가 추가되었습니다.");
+  };
+  return (
+    <form onSubmit={handleSubmit(handleAddFeed)}>
+      {/* 타이틀 인풋 영역 */}
+      <StFormTitleWrapper>
+        <h3>Title</h3>
+        <StFormTitleInput
+          type="text"
+          placeholder="제목은 최소 6자, 최대 50자를 사용하실 수 있습니다"
+          maxLength="50"
+          // react-hook-form의 register를 통해 폼 값 연결
+          {...register("title", {
+            required: true,
+            minLength: {
+              value: 6,
+              message: "※ 제목은 최소 6자 이상이어야 합니다",
+            },
+            maxLength: {
+              value: 50,
+              message: "※ 제목은 최대 50자를 초과할 수 없습니다",
+            },
+            setValueAs: (value) => value.trim(), // 입력값 양옆 공백 제거
+          })}
+        />
+        {/* 제목 입력시 발생할 수 있는 에러 메시지 */}
+        {errors.title && (
+          <p>
+            {errors.title.type === "required" && "※ 제목은 필수입니다."}
+            {errors.title.type === "minLength" &&
+              "※ 제목은 최소 6자 이상이어야 합니다"}
+            {errors.title.type === "maxLength" &&
+              "※ 제목은 최대 50자를 초과할 수 없습니다"}
+          </p>
+        )}
+      </StFormTitleWrapper>
+      {/* 본문 인풋 영역 */}
+      <StFormContentsWrapper>
+        <h3>Contents</h3>
+        <StFormContentsInput
+          type="text"
+          placeholder="내용은 최소 6자, 최대 500자를 사용하실 수 있습니다."
+          maxLength="500"
+          // react-hook-form의 register를 통해 폼 값 연결
+          {...register("contents", {
+            required: true,
+            minLength: {
+              value: 6,
+              message: "※ 내용은 최소 6자 이상이어야 합니다",
+            },
+            maxLength: {
+              value: 500,
+              message: "※ 내용은 500자를 초과할 수 없습니다",
+            },
+            setValueAs: (value) => value.trim(), // 입력값 양옆 공백 제거
+          })}
+        />
+        {/* 내용 입력시 발생할 수 있는 에러 메시지 */}
+        {errors.contents && (
+          <p>
+            {errors.contents.type === "required" && "※ 내용은 필수입니다."}
+            {errors.contents.type === "minLength" &&
+              "※ 내용은 최소 6자 이상이어야 합니다"}
+            {errors.contents.type === "maxLength" &&
+              "※ 내용은 최대 500자를 초과할 수 없습니다"}
+          </p>
+        )}
+      </StFormContentsWrapper>
+      {/* SUBMIT 버튼 영역 */}
+      <Button type="submit">글 업로드</Button>
+    </form>
+  );
+};
 
 //-----editFeedMode 게시글 수정 컴포넌트-----
 const EditFeedForm = ({ feedId }) => {
@@ -138,46 +245,10 @@ const EditFeedForm = ({ feedId }) => {
 };
 
 const FeedForm = ({ isMode, feedId }) => {
-  // 추가할 Feed 상태관리
-  const [addFeedData, setAddFeedData] = useState(INITIAL_ADD_FEED_DATA);
-  const { toggleModal } = useContext(FeedContext);
-
-  useEffect(() => {
-    // 글 업로드시 insertOrUpdateData 함수를 실행하여 테이블을 업데이트
-    if (addFeedData?.writer_id) {
-      insertOrUpdateData(addFeedData, "feeds");
-      setAddFeedData(INITIAL_ADD_FEED_DATA);
-      toggleModal();
-      alert("새로운 피드가 추가되었습니다.");
-    }
-  }, [addFeedData, toggleModal]);
-
-  // onChange시에 event와 field 객체를 받아, input value 추가
-  const handleInputChange = (e, field) => {
-    const { value } = e.target;
-    setAddFeedData((state) => ({
-      ...state,
-      [field]: value,
-    }));
-  };
-
-  // 실제 테이블에 feed 데이터 추가
-  const handleAddFeed = async (e) => {
-    e.preventDefault();
-    setAddFeedData((feed) => ({
-      ...feed,
-      writer_id: "1d4b5722-6a09-4256-9b9d-461903075838",
-    }));
-  };
-
   return (
     <>
       {isMode === "addFeedMode" ? (
-        <AddFeedForm
-          addFeedData={addFeedData}
-          handleInputChange={handleInputChange}
-          handleAddFeed={handleAddFeed}
-        />
+        <AddFeedForm />
       ) : (
         <EditFeedForm feedId={feedId} />
       )}
@@ -203,8 +274,13 @@ const StFormTitleWrapper = styled.div`
   gap: 5px;
   margin-bottom: 20px;
 
-  h1 {
+  h3 {
     font-size: 20px;
+  }
+
+  p {
+    color: tomato;
+    font-weight: 500;
   }
 `;
 
@@ -228,8 +304,13 @@ const StFormContentsWrapper = styled.div`
   gap: 5px;
   margin-bottom: 10px;
 
-  h1 {
+  h3 {
     font-size: 20px;
+  }
+
+  p {
+    color: tomato;
+    font-weight: 500;
   }
 `;
 
