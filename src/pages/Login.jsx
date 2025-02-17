0c757ba0419ad0eb3b9daa29b10bde1e02ddae11
도니 김 <kdh4847@naver.com>
2025-02-17 20:36:40 +0900

    chore: 전체 기능 test

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -44,7 +44,11 @@ const Login = () => {
         navigate("/");
       }
     } catch (error) {
-      alert(error.message);
+      AlertError(
+        "Error",
+        `회원가입 중 오류가 발생했습니다. 다시 시도해주세요!
+      ${error.massage}`
+      );
       console.log("⛔️로그인 오류", error);
     }
   };
